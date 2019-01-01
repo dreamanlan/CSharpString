3c79926cf600c4b0dac15e7066b7d928627c9098
dreamanlan <dreaman_163@163.com>
2019-01-01 15:32:48 +0800

    收集c#源代码里的字符串

---
+++ Code/CSharpCodeWalker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+
+namespace RoslynTool
+{
+    internal class CSharpCodeWalker : CSharpSyntaxWalker
+    {
+        public List<string> StringList
+        {
+            get { return m_StringList; }
+        }
+        public override void VisitLiteralExpression(LiteralExpressionSyntax node)
+        {
+            var oper = m_Model.GetOperation(node);
+            if (oper.ConstantValue.HasValue) {
+                string str = oper.ConstantValue.Value as string;
+                if (null != str) {
+
+                }
+            }
+            base.VisitLiteralExpression(node);
+        }
+
+        public CSharpCodeWalker(SemanticModel model)
+        {
+            m_Model = model;
+        }
+
+        private SemanticModel m_Model = null;
+        private List<string> m_StringList = new List<string>();
+    }
+}
---
+++ copy.bat
@@ -0,0 +1,3 @@
+copy /y bin\Debug\CSharpString.exe d:\work\Client\Tools\cs2dsl\CSharpString.exe
+
+@pause
