be94d48f024298ca17c7328da76ce1351f5aed8b
dreamanlan <dreaman_163@163.com>
2020-04-19 18:56:19 +0800

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,3 +1,4 @@
-copy /y bin\Debug\CSharpString.exe d:\work\Client\Tools\cs2dsl\CSharpString.exe
+copy /y bin\Debug\CSharpString.exe c:\Code\Client\Tools\cs2dsl\CSharpString.exe
+copy /y bin\Debug\CSharpString.exe d:\Code\Client\Tools\cs2dsl\CSharpString.exe
 
 @pause
